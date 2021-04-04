16733976395243233342d681bb61cac5a016665e
KregTM <KregTM@users.noreply.github.com>
2021-04-04 15:28:51 +0600

    csllol

---
+++ WaSd/WaSd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaSd
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string source = "88005553535";
+            for (int i = 0; i < source.Length; i++) {
+                Console.WriteLine(source[i]);
+            }
+        }
+    }
+}
